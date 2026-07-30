b3131d7ced27e0f3afd2f97d416e582f45e1d85f
VORTEX <bad969063@gmail.com>
2026-07-30 23:08:40 +0200

    v0.2 - Gestion produits (ajout, liste, recherche)

--- main.py
+++ main.py
@@ -1,20 +1,38 @@
+from app.produits import ajouter_produit, lister_produits, rechercher_produit
+
+
 def afficher_menu():
-    print("\n===SmartBiz ERP V0.1===")
-    print("1. Gestion des produits")
-    print("2. Gestion des clients")
-    print("3. Quitter")
+    print("\n===SmartBiz ERP V0.2===")
+    print("1. Ajouter un produit")
+    print("2. Lister les produits")
+    print("3. Rechercher un produit")
+    print("Quitter")
     return input("Votre choix : ")
 
 def main():
     while True:
         choix=afficher_menu()
         if choix=="1":
-            print("Module produit bientot disponible")
+            print("\n---Ajout d'un produit---")
+            nom=input("Nom du produit : ")
+            prix=input("Prix (FCFA) : ")
+
+            #Conversion du prix en nombre avec gestion d'erreur
+            try:
+                prix=float(prix)
+                ajouter_produit(nom, prix)
+            except ValueError :
+                print("Le prix doit etre un nombre valide.")
         
         elif choix=="2":
-            print("Module client (bientot disponible)")
+            lister_produits()
 
         elif choix=="3":
+            print("\n---Recherche d'un produit---")
+            nom=input("Nom a rechercher : ")
+            rechercher_produit(nom)
+
+        elif choix=="4":
             print("Au revoir !")
             break
 
